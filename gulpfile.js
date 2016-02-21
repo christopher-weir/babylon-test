07286c45427fb8df524bf94501df75052d4e4a6e
Christopher Weir <christopher_weir@me.com>
2016-02-21 16:13:07 +0000

    updated gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,15 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var html2js = require('gulp-html2js');
 
 
 var defaultAssets = {
-    less: [
-        ''
+    sass: [
+        './app/modules/*/sass/*.scss',
+        './app/modules/*/sass/*.sass'
     ],
     lib: [
         './bower_components/angular/angular.min.js',
@@ -17,11 +21,12 @@ var defaultAssets = {
         './app/modules/core/app/config.js',
         './app/modules/core/app/init.js',
         './app/modules/core/*.js',
+        './app/modules/core/app/routes.js',
         './app/modules/!(core)/*.js',
         './app/modules/*/!(app)/*.js'
     ],
     views: [
-        'modules/*/client/views/**/*.html'
+        'app/modules/*/views/*.html'
     ]
 };
 
@@ -42,10 +47,32 @@ gulp.task('buildLib', function() {
         );
 });
 
+// merge all the template html files to a js file
+gulp.task('buildTemplates', [ 'buildLib' ], function() {
+    return gulp
+        .src(
+            defaultAssets.views
+        )
+        .pipe(
+            html2js(
+                'template.js',{
+                    name: 'templates-main',
+                    useStrict: true
+                }
+            )
+        )
+        .pipe(
+            concat('template.js')
+        )
+        .pipe(
+            gulp.dest('./public/js' )
+        );
+});
+
 
 
 // Build the app files
-gulp.task('buildApp', ['buildLib'], function() {
+gulp.task('buildApp', ['buildLib','buildTemplates'], function() {
 
     return gulp
         .src(
@@ -60,8 +87,19 @@ gulp.task('buildApp', ['buildLib'], function() {
 });
 
 
+// sass task
+gulp.task('sass', function() {
+
+    return gulp
+        .src('./app/modules/core/sass/app.sass')
+        .pipe(plumber())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
+});
+
+
 // start a basic server
-gulp.task('webserver', ['buildLib', 'buildApp'], function() {
+gulp.task('webserver', ['sass', 'buildLib', 'buildApp', 'buildTemplates'], function() {
     return gulp.src('public')
         .pipe(webserver({
             livereload: true,
@@ -71,8 +109,40 @@ gulp.task('webserver', ['buildLib', 'buildApp'], function() {
 
 
 
+
+
+// Watch Files For Changes
+gulp.task('watch', ['webserver'], function() {
+
+    // watch all client sass
+    gulp.watch(
+        defaultAssets.app, [
+            'buildApp'
+        ]
+    );
+
+    // watch all client sass
+    gulp.watch(
+        defaultAssets.sass, [
+            'sass'
+        ]
+    );
+
+    // watch all client views
+    gulp.watch(
+        defaultAssets.views, [
+            'buildTemplates'
+        ]
+    );
+});
+
+
+
 gulp.task('default', [
     'buildLib',
+    'buildTemplates',
     'buildApp',
-    'webserver'
+    'sass',
+    'webserver',
+    'watch'
 ]);
